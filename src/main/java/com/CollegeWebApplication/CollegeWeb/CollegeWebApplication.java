4c14b2bc7d24599e88dc6724571d70b0635def6f
Amara3005 <amaraprasana@gmail.com>
2023-02-26 18:13:25 +0530

    Initial Commit

---
+++ src/main/java/com/CollegeWebApplication/CollegeWeb/CollegeWebApplication.java
@@ -0,0 +1,13 @@
+package com.CollegeWebApplication.CollegeWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CollegeWebApplication/CollegeWeb/CollegeWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CollegeWebApplication.CollegeWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
